337e0983dd86eb040cb243e76ef0d4320ccf77cf
BT_4N2_34 <mchoudhary4645@bths.edu>
2023-02-16 12:33:33 -0500

    add

--- src/Package.java
+++ src/Package.java
@@ -7,11 +7,16 @@ public class Package {
     private double width;
     private double height;
 
-    public Package(double weight,double l , double w, double h,){
+
+    public Package(double weight, double l , double w, double h){
       if(weight>=0.1){
           this.weight = weight;
       }
-      if(l>=2 && w>=2 &&
+      if(l>=2 && w>=2 && h>=2){
+          length = 1;
+          width = w;
+          height = h;
+      }
     }
 
     public void Addy(String origin){
@@ -21,7 +26,29 @@ public class Package {
     public void AddyDest(String dest){
         this.dest = dest;
     }
+    public String getOrigin() {
+        return origin;
+    }
 
+    public String getDest() {
+        return dest;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
 
 
 
