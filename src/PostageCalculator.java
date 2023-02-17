33e15cfb6af7882dc27583303710abe4c3933864
BT_4N2_34 <mchoudhary4645@bths.edu>
2023-02-17 12:34:01 -0500

    add

--- src/PostageCalculator.java
+++ src/PostageCalculator.java
@@ -1,2 +1,13 @@
 public class PostageCalculator {
+    private double cost;
+    public PostageCalculator(Package pack, Address addy){
+        int addres = Integer.parseInt(Integer.toString(addy.getZipcode()).substring(0,3))-100;
+        cost = 3.75 + pack.getWeight()/0.1 + addres;
+        if(pack.getLength() + pack.getHeight() + pack.getWidth() > 36){
+            cost = cost + ((36-(pack.getLength() + pack.getHeight() + pack.getWidth()))*0.10);
+        }
+        if(pack.getWeight()>40){
+            cost = cost + (40-())
+        }
+    }
 }
